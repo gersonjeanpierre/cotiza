c9176fdcd3c398f7c68a002ceeb57315daceca6d
JalukOne <jalukone@outlook.com>
2025-07-15 15:05:07 -0500

    configure ENV

--- src/environment/env.ts
+++ src/environment/env.ts
@@ -1,4 +1,5 @@
 export const ENV = {
   production: false,
-  apiUrl: 'http://127.0.1:8000/api',
+  apiUrl: 'http://127.0.0.1:8000/api', // Dev
+  // apiUrl: 'http://192.168.100.35:8000/api', // Prod
 }
